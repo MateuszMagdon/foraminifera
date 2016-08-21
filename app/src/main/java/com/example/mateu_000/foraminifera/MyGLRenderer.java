67225fb736c6e86d7956059666d10a278823e3d1
Mateusz M <mateuszm@gmail.com>
2016-08-21 03:30:26 +0200

    renderer clean-up

--- app/src/main/java/com/example/mateu_000/foraminifera/MyGLRenderer.java
+++ app/src/main/java/com/example/mateu_000/foraminifera/MyGLRenderer.java
@@ -1,7 +1,5 @@
 package com.example.mateu_000.foraminifera;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -11,160 +9,44 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.os.SystemClock;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
-/**
- * This class implements our custom renderer. Note that the GL10 parameter passed in is unused for OpenGL ES 2.0
- * renderers -- the static class GLES20 is used instead.
- */
 public class MyGLRenderer implements GLSurfaceView.Renderer
 {
-    /** Used for debug logs. */
-    private static final String TAG = "LessonTwoRenderer";
+    private static final String TAG = "MainRenderer";
 
-    /**
-     * Store the model matrix. This matrix is used to move models from object space (where each model can be thought
-     * of being located at the center of the universe) to world space.
-     */
     private float[] mModelMatrix = new float[16];
-
-    /**
-     * Store the view matrix. This can be thought of as our camera. This matrix transforms world space to eye space;
-     * it positions things relative to our eye.
-     */
     private float[] mViewMatrix = new float[16];
-
-    /** Store the projection matrix. This is used to project the scene onto a 2D viewport. */
     private float[] mProjectionMatrix = new float[16];
-
-    /** Allocate storage for the final combined matrix. This will be passed into the shader program. */
     private float[] mMVPMatrix = new float[16];
 
-    /** Store our model data in a float buffer. */
-    private final FloatBuffer mCubePositions;
-
-    /** This will be used to pass in the transformation matrix. */
     private int mMVPMatrixHandle;
-
-    /** This will be used to pass in the modelview matrix. */
     private int mMVMatrixHandle;
-
-    /** This will be used to pass in the light position. */
     private int mLightPosHandle;
-
-    /** This will be used to pass in model position information. */
     private int mPositionHandle;
 
-    /** How many bytes per float. */
-    private final int mBytesPerFloat = 4;
-
-    /** Size of the position data in elements. */
     private final int mPositionDataSize = 3;
 
-    /** Used to hold a light centered on the origin in model space. We need a 4th coordinate so we can get translations to work when
-     *  we multiply this by our transformation matrices. */
     private final float[] mLightInitialPosition = new float[] {0.0f, 0.0f, 0.0f, 1.0f};
-
-    /** Used to hold the current position of the light in world space (after transformation via model matrix). */
     private final float[] mLightCalculatedPosition = new float[4];
-
-    /** Used to hold the transformed position of the light in eye space (after transformation via modelview matrix) */
     private final float[] mLightPosInEyeSpace = new float[4];
 
-    /** This is a handle to our per-vertex cube shading program. */
     private int mPerVertexProgramHandle;
-
-    /** This is a handle to our light point program. */
-    private int mPointProgramHandle;
-
-    /**
-     * Initialize the model data.
-     */
-    public MyGLRenderer()
-    {
-        final float[] cubePositionData = GetCubePositionData();
-
-        // Initialize the buffers.
-        mCubePositions = ByteBuffer.allocateDirect(cubePositionData.length * mBytesPerFloat)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mCubePositions.put(cubePositionData).position(0);
-    }
-
-    private float[] GetCubePositionData() {
-        // Define points for a cube.
-
-        // X, Y, Z
-        return new float[]{
-                // In OpenGL counter-clockwise winding is default. This means that when we look at a triangle,
-                // if the points are counter-clockwise we are looking at the "front". If not we are looking at
-                // the back. OpenGL has an optimization where all back-facing triangles are culled, since they
-                // usually represent the backside of an object and aren't visible anyways.
-
-                // Front face
-                -1.0f, 1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-
-                // Right face
-                1.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f,
-                1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, -1.0f,
-                1.0f, 1.0f, -1.0f,
-
-                // Back face
-                1.0f, 1.0f, -1.0f,
-                1.0f, -1.0f, -1.0f,
-                -1.0f, 1.0f, -1.0f,
-                1.0f, -1.0f, -1.0f,
-                -1.0f, -1.0f, -1.0f,
-                -1.0f, 1.0f, -1.0f,
-
-                // Left face
-                -1.0f, 1.0f, -1.0f,
-                -1.0f, -1.0f, -1.0f,
-                -1.0f, 1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f,
-                -1.0f, -1.0f, 1.0f,
-                -1.0f, 1.0f, 1.0f,
-
-                // Top face
-                -1.0f, 1.0f, -1.0f,
-                -1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f,
-                -1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f,
-
-                // Bottom face
-                1.0f, -1.0f, -1.0f,
-                1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f,
-                1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f,
-        };
-    }
 
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config)
     {
-        // Set the background clear color to black.
         GLES20.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
-        // Use culling to remove back faces.
         GLES20.glEnable(GLES20.GL_CULL_FACE);
-
-        // Enable depth testing
         GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 
-        SetProjectionMatrix();
+        setProjectionMatrix();
 
+        compileShaders();
+    }
+
+    private void compileShaders() {
         final String vertexShader = getVertexShader();
         final String fragmentShader = getFragmentShader();
 
@@ -172,15 +54,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
         final int fragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
 
         mPerVertexProgramHandle = createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle,
-                new String[] {"a_Position"});
-
-        final String pointVertexShader = getSimpleVertexShader();
-        final String pointFragmentShader = getSimpleFragmentShader();
-
-        final int pointVertexShaderHandle = compileShader(GLES20.GL_VERTEX_SHADER, pointVertexShader);
-        final int pointFragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, pointFragmentShader);
-
-        mPointProgramHandle = createAndLinkProgram(pointVertexShaderHandle, pointFragmentShaderHandle,
                 new String[] {"a_Position"});
     }
 
@@ -231,27 +104,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
         return fragmentShader;
     }
 
-    private String getSimpleVertexShader() {
-        return "uniform mat4 u_MVPMatrix;      \n"
-                +	"attribute vec4 a_Position;     \n"
-                + "void main()                    \n"
-                + "{                              \n"
-                + "   gl_Position = u_MVPMatrix   \n"
-                + "               * a_Position;   \n"
-                + "   gl_PointSize = 20.0;         \n"
-                + "}                              \n";
-    }
-
-    private String getSimpleFragmentShader() {
-        return "precision mediump float;       \n"
-                + "void main()                    \n"
-                + "{                              \n"
-                + "   gl_FragColor = vec4(1.0,    \n"
-                + "   0.0, 0.0, 0.0);             \n"
-                + "}                              \n";
-    }
-
-    private void SetProjectionMatrix() {
+    private void setProjectionMatrix() {
         // Position the eye in front of the origin.
         final float eyeX = 0.0f;
         final float eyeY = 0.0f;
@@ -296,21 +149,31 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
     public void onDrawFrame(GL10 glUnused)
     {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-        // Do a complete rotation every 10 seconds.
-        long time = SystemClock.uptimeMillis() % 10000L;
-        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
-        float lightRorationAngle = (360.0f / 1000.0f) * ((int) time);
-
-        // Set our per-vertex lighting program.
         GLES20.glUseProgram(mPerVertexProgramHandle);
 
-        // Set program handles for cube drawing.
+        prepareShaderInputHandlers();
+
+        long time = SystemClock.uptimeMillis() % 10000L;
+        float lightRorationAngle = (360.0f / 1000.0f) * ((int) time);
+        iluminateScene(lightRorationAngle);
+
+        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -7.0f);
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
+        drawSphere();
+
+    }
+
+    private void prepareShaderInputHandlers() {
         mMVPMatrixHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_MVPMatrix");
         mMVMatrixHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_MVMatrix");
         mLightPosHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_LightPos");
         mPositionHandle = GLES20.glGetAttribLocation(mPerVertexProgramHandle, "a_Position");
+    }
 
+    private void iluminateScene(float lightRorationAngle) {
         // Calculate position of the light. Rotate and then push into the distance.
         Matrix.setIdentityM(mModelMatrix, 0);
         Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -5.0f);
@@ -319,31 +182,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
 
         Matrix.multiplyMV(mLightCalculatedPosition, 0, mModelMatrix, 0, mLightInitialPosition, 0);
         Matrix.multiplyMV(mLightPosInEyeSpace, 0, mViewMatrix, 0, mLightCalculatedPosition, 0);
-
-        // Draw some cubes.
-//        Matrix.setIdentityM(mModelMatrix, 0);
-//        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -7.0f);
-//        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-//        Matrix.translateM(mModelMatrix, 0, 0.0f, 4.0f, 0.0f);
-//        Matrix.rotateM(mModelMatrix, 0, -2*angleInDegrees, 1.0f, 1.0f, 1.0f);
-//        drawCube();
-//
-//
-//        Matrix.setIdentityM(mModelMatrix, 0);
-//        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -5.0f);
-//        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 1.0f, 1.0f, 0.0f);
-//        drawCube();
-
-
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -4.0f);
-        drawSphere();
-
-        // Draw a point to indicate the light.
-        //GLES20.glUseProgram(mPointProgramHandle);
-        //drawLight();
     }
-
 
     public void drawSphere() {
         //SphereVertices sphere = new SphereVertices(2.0f, 3);
@@ -365,58 +204,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, sphere.pointsCount);
     }
 
-
-    private void drawCube()
-    {
-        // Pass in the position information
-        mCubePositions.position(0);
-        GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
-                0, mCubePositions);
-        GLES20.glEnableVertexAttribArray(mPositionHandle);
-
-        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
-        // (which currently contains model * view).
-        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
-
-        // Pass in the modelview matrix.
-        GLES20.glUniformMatrix4fv(mMVMatrixHandle, 1, false, mMVPMatrix, 0);
-
-        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
-        // (which now contains model * view * projection).
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
-
-        // Pass in the combined matrix.
-        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-
-        // Pass in the light position in eye space.
-        GLES20.glUniform3f(mLightPosHandle, mLightPosInEyeSpace[0], mLightPosInEyeSpace[1], mLightPosInEyeSpace[2]);
-
-        // Draw the cube.
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 36);
-    }
-
-    /**
-     * Draws a point representing the position of the light.
-     */
-    private void drawLight()
-    {
-        final int pointMVPMatrixHandle = GLES20.glGetUniformLocation(mPointProgramHandle, "u_MVPMatrix");
-        final int pointPositionHandle = GLES20.glGetAttribLocation(mPointProgramHandle, "a_Position");
-
-        // Pass in the position.
-        GLES20.glVertexAttrib3f(pointPositionHandle, mLightInitialPosition[0], mLightInitialPosition[1], mLightInitialPosition[2]);
-
-        // Since we are not using a buffer object, disable vertex arrays for this attribute.
-        GLES20.glDisableVertexAttribArray(pointPositionHandle);
-
-        // Pass in the transformation matrix.
-        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
-        GLES20.glUniformMatrix4fv(pointMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-
-        // Draw the point.
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
-    }
 
     /**
      * Helper function to compile a shader.
@@ -512,133 +299,3 @@ public class MyGLRenderer implements GLSurfaceView.Renderer
         return programHandle;
     }
 }
-
-
-
-
-/*
-
-import android.opengl.GLSurfaceView.Renderer;
-import android.opengl.GLU;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
-
-public class MyGLRenderer implements Renderer{
-    // Ambient light
-    private final float[] mat_ambient = { 0.2f, 0.3f, 0.4f, 1.0f };
-    private FloatBuffer mat_ambient_buf;
-    // Parallel incident light
-    private final float[] mat_diffuse = { 0.4f, 0.6f, 0.8f, 1.0f };
-    private FloatBuffer mat_diffuse_buf;
-    // The highlighted area
-    private final float[] mat_specular = { 0.2f * 0.4f, 0.2f * 0.6f, 0.2f * 0.8f, 1.0f };
-    private FloatBuffer mat_specular_buf;
-
-    private SphereVertices mSphere = new SphereVertices(10, 0.2f);
-    //private SphereVertices mSphere2 = new SphereVertices();
-
-    public volatile float mLightX = 10f;
-    public volatile float mLightY = 10f;
-    public volatile float mLightZ = 10f;
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        // To clear the screen and the depth buffer
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        // Reset the modelview matrix
-        gl.glLoadIdentity();
-
-        gl.glEnable(GL10.GL_LIGHTING);
-        gl.glEnable(GL10.GL_LIGHT0);
-
-        // Texture of material
-        gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, mat_ambient_buf);
-        gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_DIFFUSE, mat_diffuse_buf);
-        gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, mat_specular_buf);
-        // Specular exponent 0~128 less rough
-        gl.glMaterialf(GL10.GL_FRONT_AND_BACK, GL10.GL_SHININESS, 96.0f);
-
-        //The position of the light source
-        float[] light_position = {mLightX, mLightY, mLightZ, 0.0f};
-        ByteBuffer mpbb = ByteBuffer.allocateDirect(light_position.length*4);
-        mpbb.order(ByteOrder.nativeOrder());
-        FloatBuffer mat_posiBuf = mpbb.asFloatBuffer();
-        mat_posiBuf.put(light_position);
-        mat_posiBuf.position(0);
-        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, mat_posiBuf);
-
-        gl.glTranslatef(0.0f, 0.0f, -3.0f);
-        mSphere.draw(gl);
-
-        //gl.glTranslatef(0.0f, 10.0f, 0.0f);
-        //mSphere2.draw(gl, 20f);
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-
-        // Set the output screen size
-        gl.glViewport(0, 0, width, height);
-
-        // Projection matrix
-        gl.glMatrixMode(GL10.GL_PROJECTION);
-        // Reset the projection matrix
-        gl.glLoadIdentity();
-        // Set the viewport size
-        // gl.glFrustumf(0, width, 0, height, 0.1f, 100.0f);
-
-        GLU.gluPerspective(gl, 90.0f, (float) width / height, 0.1f, 50.0f);
-
-        // Select the model view matrix
-        gl.glMatrixMode(GL10.GL_MODELVIEW);
-        // Reset the modelview matrix
-        gl.glLoadIdentity();
-
-    }
-
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig arg1) {
-        // On the perspective correction
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
-        // Background: Black
-        gl.glClearColor(0, 0.0f, 0.0f, 0.0f);
-        // Start the smooth shading
-        gl.glShadeModel(GL10.GL_SMOOTH);
-
-        // Reset the depth buffer
-        gl.glClearDepthf(1.0f);
-        // Start the depth test
-        gl.glEnable(GL10.GL_DEPTH_TEST);
-        // Type the depth test
-        gl.glDepthFunc(GL10.GL_LEQUAL);
-
-        initBuffers();
-    }
-
-    private void initBuffers() {
-        ByteBuffer bufTemp = ByteBuffer.allocateDirect(mat_ambient.length * 4);
-        bufTemp.order(ByteOrder.nativeOrder());
-        mat_ambient_buf = bufTemp.asFloatBuffer();
-        mat_ambient_buf.put(mat_ambient);
-        mat_ambient_buf.position(0);
-
-        bufTemp = ByteBuffer.allocateDirect(mat_diffuse.length * 4);
-        bufTemp.order(ByteOrder.nativeOrder());
-        mat_diffuse_buf = bufTemp.asFloatBuffer();
-        mat_diffuse_buf.put(mat_diffuse);
-        mat_diffuse_buf.position(0);
-
-        bufTemp = ByteBuffer.allocateDirect(mat_specular.length * 4);
-        bufTemp.order(ByteOrder.nativeOrder());
-        mat_specular_buf = bufTemp.asFloatBuffer();
-        mat_specular_buf.put(mat_specular);
-        mat_specular_buf.position(0);
-    }
-}
-
- */
