f4c6f592f788b20295795000e08dfd3294ce0baa
Mateusz M <mateuszm@gmail.com>
2016-08-23 20:39:16 +0200

    Shaders extracted to separate container

--- app/src/main/java/com/example/mateu_000/foraminifera/MyGLRenderer.java
+++ app/src/main/java/com/example/mateu_000/foraminifera/MyGLRenderer.java
@@ -31,8 +31,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private float[] mMVPMatrix = new float[16];
 
     private float[] mAccumulatedRotation = new float[16];
-    private float[] mCurrentRotation = new float[16];
-    private float[] mCurrentTranslation = new float[16];
 
     private final float[] mLightInitialPosition = new float[] {0.0f, 0.0f, 0.0f, 1.0f};
     private final float[] mLightCalculatedPosition = new float[4];
@@ -58,61 +56,14 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void compileShaders() {
-        final String vertexShader = getVertexShader();
-        final String fragmentShader = getFragmentShader();
+        final String vertexShader = ShadersContainer.vertexShader;
+        final String fragmentShader = ShadersContainer.fragmentShader;
 
         final int vertexShaderHandle = compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
         final int fragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
 
         mPerVertexProgramHandle = createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle,
                 new String[] {"a_Position"});
-    }
-
-    private String getVertexShader() {
-        final String vertexShader =
-                "uniform mat4 u_MVPMatrix;      \n"		// A constant representing the combined model/view/projection matrix.
-                        + "uniform mat4 u_MVMatrix;       \n"		// A constant representing the combined model/view matrix.
-                        + "uniform vec3 u_LightPos;       \n"	    // The position of the light in eye space.
-                        + "attribute vec4 a_Position;     \n"		// Per-vertex position information we will pass in.
-                        + "varying vec4 v_Color;          \n"		// This will be passed into the fragment shader.
-
-                        + "void main()                    \n" 	// The entry point for our vertex shader.
-                        + "{                              \n"
-                        // Transform the vertex into eye space.
-                        + "   vec3 modelViewVertex = vec3(u_MVMatrix * a_Position);              \n"
-                        // Transform the normal's orientation into eye space.
-                        + "   vec3 modelViewNormal = vec3(u_MVMatrix);     \n"
-                        // Will be used for attenuation.
-                        + "   float distance = length(u_LightPos - modelViewVertex);             \n"
-                        // Get a lighting direction vector from the light to the vertex.
-                        + "   vec3 lightVector = normalize(u_LightPos - modelViewVertex);        \n"
-                        // Calculate the dot product of the light vector and vertex normal. If the normal and light vector are
-                        // pointing in the same direction then it will get max illumination.
-                        + "   float diffuse = max(dot(modelViewNormal, lightVector), 2.5f);       \n"
-                        // Attenuate the light based on distance.
-                        + "   diffuse = diffuse * (1.0 / (1.0 + (0.2 * distance * distance)));  \n"
-                        // Multiply the color by the illumination level. It will be interpolated across the triangle.
-                        + "   v_Color = vec4(1.0, 0.498039, 0.0, 1.0) * diffuse; \n"
-                        // gl_Position is a special variable used to store the final position.
-                        // Multiply the vertex by the matrix to get the final point in normalized screen coordinates.
-                        + "   gl_Position = u_MVPMatrix * a_Position;                            \n"
-                        + "}                                                                     \n";
-
-        return vertexShader;
-    }
-
-    private String getFragmentShader() {
-        final String fragmentShader =
-                "precision mediump float;       \n"		// Set the default precision to medium. We don't need as high of a
-                        // precision in the fragment shader.
-                        + "varying vec4 v_Color;          \n"		// This is the color from the vertex shader interpolated across the
-                        // triangle per fragment.
-                        + "void main()                    \n"		// The entry point for our fragment shader.
-                        + "{                              \n"
-                        + "   gl_FragColor = v_Color;     \n"		// Pass the color directly through the pipeline.
-                        + "}                              \n";
-
-        return fragmentShader;
     }
 
     private void setProjectionMatrix() {
@@ -155,7 +106,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
 
         Matrix.setIdentityM(mAccumulatedRotation, 0);
-        Matrix.setIdentityM(mCurrentTranslation, 0);
     }
 
     @Override
@@ -230,6 +180,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void handleRotation() {
+        float[] mCurrentRotation = new float[16];
+
         Matrix.setIdentityM(mCurrentRotation, 0);
         Matrix.rotateM(mCurrentRotation, 0, mDeltaRotationX, 0.0f, 1.0f, 0.0f);
         Matrix.rotateM(mCurrentRotation, 0, mDeltaRotationY, 1.0f, 0.0f, 0.0f);
@@ -241,6 +193,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void handleTranslation() {
+        float[] mCurrentTranslation = new float[16];
+
         Matrix.setIdentityM(mCurrentTranslation, 0);
         Matrix.translateM(mCurrentTranslation, 0, mDeltaTranslationX, -mDeltaTranslationY, 0.0f);
         mDeltaTranslationX = 0.0f;
