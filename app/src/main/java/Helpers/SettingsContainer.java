0833601893d42a70e17d766058bf64a62bdeec0c
Mateusz M <mateuszm@gmail.com>
2016-09-03 18:00:31 +0200

    removed unused settings default values

--- app/src/main/java/Helpers/SettingsContainer.java
+++ app/src/main/java/Helpers/SettingsContainer.java
@@ -1,17 +1,17 @@
 package Helpers;
 
 public class SettingsContainer {
-    public static double numberOfChambers = 3;
+    public static double numberOfChambers;
 
-    public static double translationFactor = 0.8; //przesunięcie po growth axis srodka kolejnej sfery wzgledem srodka poprzedniej
+    public static double translationFactor; //przesunięcie po growth axis srodka kolejnej sfery wzgledem srodka poprzedniej
         //0 to centrum kolejnej sfery w apperture kolejnej
         //1 to stykające się sfery - v = newRadius
 
-    public static double growthFactor = 1.4; //scaling rate of growth vector and radius
-    public static double thicknessGrowthFactor = 1.1; //scaling rate of thickness
+    public static double growthFactor; //scaling rate of growth vector and radius
+    public static double thicknessGrowthFactor; //scaling rate of thickness
 
-    public static double deviationAngle = Math.PI / 10;
-    public static double rotationAngle = Math.PI / 10;
+    public static double deviationAngle;
+    public static double rotationAngle;
 
     //additional scaling;
     public static double scaleX = 1.0;
